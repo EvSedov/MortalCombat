ba0789642208d0d6c9d55f5350c56c43b343dee7
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-04-08 22:47:09 +0700

    Task #1

--- main.js
+++ main.js
@@ -18,3 +18,32 @@ const player2 = {
   }
 };
 
+const createPlayer = function () {
+  const $divPlayer1 = document.createElement('div')
+  $divPlayer1.classList.add('player1');
+  const $divProgressbar = document.createElement('div')
+  $divProgressbar.classList.add('progressbar');
+  const $divCharacter = document.createElement('div')
+  $divCharacter.classList.add('character');
+  const $divLife = document.createElement('div');
+  $divLife.classList.add('life');
+  $divLife.style.width = '100%';
+  const $divName = document.createElement('div')
+  $divName.classList.add('name');
+  $divName.innerText = player1.name;
+
+  const $img = document.createElement('img');
+  $img.src = player1.img;
+
+  $divProgressbar.appendChild($divLife)
+  $divProgressbar.appendChild($divName);
+  $divCharacter.appendChild($img);
+  $divPlayer1.appendChild($divProgressbar)
+  $divPlayer1.appendChild($divCharacter);
+
+  const $divArenas = document.querySelector('.arenas');
+  $divArenas.appendChild($divPlayer1);
+  
+};
+
+createPlayer();
