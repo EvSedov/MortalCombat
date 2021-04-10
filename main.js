b05d3d97ddcffd5f731ef2b7c38766bff36eb514
Evgeniy Sedov <sedoff-online@yandex.ru>
2021-04-11 04:35:39 +0700

    Tasks #1, #2

--- main.js
+++ main.js
@@ -60,10 +60,13 @@ function playerLose(name) {
 
 function changeHP(player) {
   const $playerLife = document.querySelector(`.player${player.player} .life`);
-  player.hp -= 20;
+  const damage = Math.floor(Math.random() * 21);
+  player.hp -= damage;  
   $playerLife.style.width = player.hp + '%';
 
-  if (player.hp < 0) {
+  if (player.hp <= 0) {
+    player.hp = 0;
+    $playerLife.style.width = player.hp + '%';
     $divArenas.appendChild(playerLose(player.name));
   }
 };
